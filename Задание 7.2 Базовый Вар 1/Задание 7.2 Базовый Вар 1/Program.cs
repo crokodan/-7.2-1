425c441266f0ec706565951b26e0a3bbfaf9ad9a
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 19:15:31 +0400

    Add files via upload

---
+++ Задание 7.2 Базовый Вар 1/Задание 7.2 Базовый Вар 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7._2_Базовый_Вар_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символы, содержащие букву \"А\"");
+            string s = Console.ReadLine();
+            s = s.ToUpper();
+            int k = 0;
+            foreach (char c in s)
+            {
+                if (c == 'А') k++;
+            }
+            Console.WriteLine("Количество букв А=" + k);
+        }
+    }
+}
